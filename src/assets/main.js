2c1ff8767400d977a6a5d546a6d19932620cb9f8
w01k3 <wolkenart@gmail.com>
2017-03-05 18:27:34 +0100

    feat(main.js): Add course title to elements

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,10 +4,10 @@ $(function() {
     url: 'https://www.codeschool.com/users/w01k3.json',
     dataType: 'jsonp',
     success: function(response) {
-      // console.log(response.courses.completed)
+      console.log(response.courses.completed);
       try {
         for (let c = 0; c < $(response.courses.completed).length; c++) {
-          $('#badges').append('<div class="course"></div>')
+          $('#badges').append('<div class="course"><h3>' + response.courses.completed[c].title + '</h3></div>');
         }
       } catch(e) {
         console.log(e)
